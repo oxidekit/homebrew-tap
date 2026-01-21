2e62415b2a0fdd3316858cb9497ce6156fe9f93c
Nayiem Willems <nayiem@qredit.io>
2026-01-21 14:53:41 -0800

    Update oxidepm to v0.1.1

--- Formula/oxidepm.rb
+++ Formula/oxidepm.rb
@@ -4,25 +4,25 @@
 class Oxidepm < Formula
   desc "Fast, modern process manager for Node.js and Rust applications"
   homepage "https://github.com/oxidekit/oxidepm"
-  version "0.1.0"
+  version "0.1.1"
   license "MIT"
 
   on_macos do
     on_arm do
       url "https://github.com/oxidekit/oxidepm/releases/download/v#{version}/oxidepm-aarch64-apple-darwin.tar.gz"
-      sha256 "22c6e9d03478e5b332071c151ba55ed28a2274bd58f6308e281cbcddce34c2a5"
+      sha256 "d88fd3edbea2b9529fc996f4b941ea5dd53959233392fc84ac500b774266c05e"
     end
   end
 
   on_linux do
     on_arm do
       url "https://github.com/oxidekit/oxidepm/releases/download/v#{version}/oxidepm-aarch64-unknown-linux-gnu.tar.gz"
-      sha256 "8538b3e24fb90635f7f77352323ee5db205fe270a78c98d05c661066fdf6b2f9"
+      sha256 "ffea93b0b5552128ff801677fb59690635b3ed70082245d0eeeab465fbade9d7"
     end
 
     on_intel do
       url "https://github.com/oxidekit/oxidepm/releases/download/v#{version}/oxidepm-x86_64-unknown-linux-gnu.tar.gz"
-      sha256 "117a1318375573b4e7033cb6e6898444eab097ecf79a217fdc3ae0a03100c916"
+      sha256 "21b86a45e1f37385f66b6cf0fa161253f8d93fe6b69194ddf06ea8fe8c778949"
     end
   end
 
