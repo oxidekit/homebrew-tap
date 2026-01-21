a210d6d55401807832cd6f0f85bf5f7cc8d2a97f
Nayiem Willems <nayiem@qredit.io>
2026-01-21 14:40:28 -0800

    Update oxidepm to v0.1.0 with real checksums

--- Formula/oxidepm.rb
+++ Formula/oxidepm.rb
@@ -10,45 +10,25 @@ class Oxidepm < Formula
   on_macos do
     on_arm do
       url "https://github.com/oxidekit/oxidepm/releases/download/v#{version}/oxidepm-aarch64-apple-darwin.tar.gz"
-      sha256 "PLACEHOLDER_AARCH64_DARWIN"
-
-      def install
-        bin.install "oxidepm"
-        bin.install "oxidepmd"
-      end
-    end
-
-    on_intel do
-      url "https://github.com/oxidekit/oxidepm/releases/download/v#{version}/oxidepm-x86_64-apple-darwin.tar.gz"
-      sha256 "PLACEHOLDER_X86_64_DARWIN"
-
-      def install
-        bin.install "oxidepm"
-        bin.install "oxidepmd"
-      end
+      sha256 "22c6e9d03478e5b332071c151ba55ed28a2274bd58f6308e281cbcddce34c2a5"
     end
   end
 
   on_linux do
     on_arm do
       url "https://github.com/oxidekit/oxidepm/releases/download/v#{version}/oxidepm-aarch64-unknown-linux-gnu.tar.gz"
-      sha256 "PLACEHOLDER_AARCH64_LINUX"
-
-      def install
-        bin.install "oxidepm"
-        bin.install "oxidepmd"
-      end
+      sha256 "8538b3e24fb90635f7f77352323ee5db205fe270a78c98d05c661066fdf6b2f9"
     end
 
     on_intel do
       url "https://github.com/oxidekit/oxidepm/releases/download/v#{version}/oxidepm-x86_64-unknown-linux-gnu.tar.gz"
-      sha256 "PLACEHOLDER_X86_64_LINUX"
-
-      def install
-        bin.install "oxidepm"
-        bin.install "oxidepmd"
-      end
+      sha256 "117a1318375573b4e7033cb6e6898444eab097ecf79a217fdc3ae0a03100c916"
     end
+  end
+
+  def install
+    bin.install "oxidepm"
+    bin.install "oxidepmd"
   end
 
   test do
